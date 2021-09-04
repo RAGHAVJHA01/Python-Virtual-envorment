4cff9f6cfa88277e4b3a08e79022c58828e39fe0
Raghav jha <46911401+RAGHAVJHA01@users.noreply.github.com>
2021-09-04 21:32:19 +0530

    Create Py_virtual_env.py

---
+++ Py_virtual_env.py
@@ -0,0 +1,8 @@
+#install virtual envorment
+pip install virtualenv
+#Create a virtual environment, you must specify a path. For example to create one in the local directory called ‘mypython’, type the following:
+virtualenv mypython
+#Activate virtual Env
+mypthon\Scripts\activate
+#Deactivate the virtual environment
+deactivate
